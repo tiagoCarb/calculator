eca09b3c419c11c36d2747cc839d4ee3a866c52a
tiagoCarb <tiagocarbonelli@gmail.com>
2025-08-02 22:35:23 -0300

    Create add, subtract, multiply and divide functions

---
+++ script.js
@@ -0,0 +1,16 @@
+const add = function (numOne, numTwo) {
+  return numOne + numTwo;
+};
+
+const subtract = function (numOne, numTwo) {
+  return numOne - numTwo;
+};
+
+const multiply = function (numOne, numTwo) {
+  return numOne * numTwo;
+};
+
+const divide = function (numOne, numTwo) {
+  return numOne / numTwo;
+};
+
